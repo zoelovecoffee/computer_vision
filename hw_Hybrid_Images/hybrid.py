500838bf124186bc8b336e4441d2734c900e65c2
zoelovecoffee <86939961+zoelovecoffee@users.noreply.github.com>
2021-07-08 13:38:44 +0800

    Add files via upload

---
+++ hw_Hybrid_Images/hybrid.py
@@ -0,0 +1,146 @@
+import sys
+import cv2
+import numpy as np
+import math
+
+def cross_correlation_2d(img, kernel):
+    '''Given a kernel of arbitrary m x n dimensions, with both m and n being
+    odd, compute the cross correlation of the given image with the given
+    kernel, such that the output is of the same dimensions as the image and that
+    you assume the pixels out of the bounds of the image to be zero. Note that
+    you need to apply the kernel to each channel separately, if the given image
+    is an RGB image.
+
+    Inputs:
+        img:    Either an RGB image (height x width x 3) or a grayscale image
+                (height x width) as a numpy array.
+        kernel: A 2D numpy array (m x n), with m and n both odd (but may not be
+                equal).
+
+    Output:
+        Return an image of the same dimensions as the input image (same width,
+        height and the number of color channels)
+    '''
+    # TODO-BLOCK-BEGIN
+    row = img.shape[0]
+    col = img.shape[1]
+    m = kernel.shape[0]
+    n = kernel.shape[1]
+    p = m/2
+    q = n/2
+    new_img = np.zeros(img.shape)
+    for i in range(row):
+        for j in range(col):
+            for u in range(-p, p+1, 1):
+                for v in range(-q, q+1, 1):
+                    if 0 <= i+u < row and 0 <= j+v < col:
+                        new_img[i, j] = new_img[i, j] + np.dot(kernel[p+u, q+v], img[i+u, j+v])
+    return new_img
+    # TODO-BLOCK-END
+
+def convolve_2d(img, kernel):
+    '''Use cross_correlation_2d() to carry out a 2D convolution.
+
+    Inputs:
+        img:    Either an RGB image (height x width x 3) or a grayscale image
+                (height x width) as a numpy array.
+        kernel: A 2D numpy array (m x n), with m and n both odd (but may not be
+                equal).
+
+    Output:
+        Return an image of the same dimensions as the input image (same width,
+        height and the number of color channels)
+    '''
+    # TODO-BLOCK-BEGIN
+    new_img = cross_correlation_2d(img, np.flip(kernel))
+    return new_img
+    # TODO-BLOCK-END
+
+def gaussian_blur_kernel_2d(sigma, height, width):
+    '''Return a Gaussian blur kernel of the given dimensions and with the given
+    sigma. Note that width and height are different.
+
+    Input:
+        sigma:  The parameter that controls the radius of the Gaussian blur.
+                Note that, in our case, it is a circular Gaussian (symmetric
+                across height and width).
+        width:  The width of the kernel.
+        height: The height of the kernel.
+
+    Output:
+        Return a kernel of dimensions height x width such that convolving it
+        with an image results in a Gaussian-blurred image.
+    '''
+    # TODO-BLOCK-BEGIN
+    p = height/2
+    q = width/2
+    kernel_xy = np.zeros((height, width))
+    for i in range(-p, p + 1, 1):
+        for j in range(-q, q + 1, 1):
+            kernel_xy[i+p, j+q] = -(i * i + j * j)/(2.0 * sigma * sigma)
+
+    kernel_temp = np.exp(kernel_xy)
+    kernel_sum = np.sum(kernel_temp)
+    kernel = kernel_temp / kernel_sum
+
+    return kernel
+    # TODO-BLOCK-END
+
+def low_pass(img, sigma, size):
+    '''Filter the image as if its filtered with a low pass filter of the given
+    sigma and a square kernel of the given size. A low pass filter surpresses
+    the higher frequency components (finer details) of the image.
+
+    Output:
+        Return an image of the same dimensions as the input image (same width,
+        height and the number of color channels)
+    '''
+    # TODO-BLOCK-BEGIN
+    kernel = gaussian_blur_kernel_2d(sigma, size, size)
+    new_image = convolve_2d(img, kernel)
+    return new_image
+    # TODO-BLOCK-END
+
+def high_pass(img, sigma, size):
+    '''Filter the image as if its filtered with a high pass filter of the given
+    sigma and a square kernel of the given size. A high pass filter suppresses
+    the lower frequency components (coarse details) of the image.
+
+    Output:
+        Return an image of the same dimensions as the input image (same width,
+        height and the number of color channels)
+    '''
+    # TODO-BLOCK-BEGIN
+    # orig = np.ones((size, size))
+    kernel = gaussian_blur_kernel_2d(sigma, size, size)
+    smoothed = convolve_2d(img, kernel)
+    new_image = img - smoothed
+    return new_image
+    # TODO-BLOCK-END
+
+def create_hybrid_image(img1, img2, sigma1, size1, high_low1, sigma2, size2,
+        high_low2, mixin_ratio, scale_factor):
+    '''This function adds two images to create a hybrid image, based on
+    parameters specified by the user.'''
+    high_low1 = high_low1.lower()
+    high_low2 = high_low2.lower()
+
+    if img1.dtype == np.uint8:
+        img1 = img1.astype(np.float32) / 255.0
+        img2 = img2.astype(np.float32) / 255.0
+
+    if high_low1 == 'low':
+        img1 = low_pass(img1, sigma1, size1)
+    else:
+        img1 = high_pass(img1, sigma1, size1)
+
+    if high_low2 == 'low':
+        img2 = low_pass(img2, sigma2, size2)
+    else:
+        img2 = high_pass(img2, sigma2, size2)
+
+    img1 *=  (1 - mixin_ratio)
+    img2 *= mixin_ratio
+    hybrid_img = (img1 + img2) * scale_factor
+    return (hybrid_img * 255).clip(0, 255).astype(np.uint8)
+
